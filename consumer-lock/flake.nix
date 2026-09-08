0d823b5e1def5909432c2dfb2ad52ec276bc4a95
RJ <rybertjen@gmail.com>
2026-09-08 10:11:13 -0700

    chore: derive Dubnium lock for Invokrum v0.2.1

--- consumer-lock/flake.nix
+++ consumer-lock/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/c5c4a43b0e8056328ec4529f735cabdb8f1942bb";
     invokrum = {
-      url = "github:hackelia-micrantha/invokrum-community/f8626e88978159aba52388af83f63738bdf38c58";
+      url = "github:hackelia-micrantha/invokrum-community/v0.2.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
